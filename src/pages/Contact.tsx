0cac4369b451ab3123ad7f33a1a8deb963139c76
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-10-08 17:04:46 +0200

    :FEAT: => Mise en place partie contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,33 +1,49 @@
-
 //- ########################################################
 //- --------------------- IMPORT ---------------------------
-//- ######################################################## 
-import React  from 'react';
-import Navbar from '../components/Navbar';
+//- ########################################################
+import React from "react";
+import Navbar from "../components/Navbar";
 //- --------------------------------------------------------
 //- ########################################################
-
-
 
 //* ########################################################
 //* ----------------- APP => CONTACT -----------------------
 //* ########################################################
 const Contact = () => {
-    return (
-        <div>
-            <Navbar />
-            Contact   
+  return (
+    <div>
+      <Navbar />
+      <div className="img-contact">
+        <img
+          src="https://github.com/Anthonoir576/Projet-01_PRO/blob/master/public/assets/images/photo-profil.jpg?raw=true"
+          alt="profil Emelyne Wojdowski"
+        />
+      </div>
+      <div className="information-contact">
+        <div className="information-container">
+          <div className="information-icon">
+            
+          </div>
+          <div className="information-txt">
+            <p>06 64 23 45 67</p>
+            <p>wojdowskiemy@gmail.com</p>
+            <p>Emelyne WOJDOWSKI</p>
+            <p>
+              Montigny-En-Ostrevent, 59182, <br />
+              HAUT-DE-FRANCE, FRANCE
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 //* --------------------------------------------------------
 //* ########################################################
 
-
-
 //~ ########################################################
 //~ --------------------- EXPORT ---------------------------
-//~ ######################################################## 
+//~ ########################################################
 export default Contact;
 //~ --------------------------------------------------------
 //~ ########################################################
