fba497ad2fcd1689281f04e6947ae6c71560180f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-10-09 12:35:42 +0200

    :FEAT: => Mise en place partie exp

--- src/pages/Exp.tsx
+++ src/pages/Exp.tsx
@@ -102,68 +102,97 @@ const Exp = () => {
           </div>
           <div className="exp-main">
             <div className="exp-date">
-            {" "}
+              {" "}
               <p>
                 04/2014 <br /> 03/2014{" "}
               </p>
             </div>
             <div className="exp-content">
-              <div className="exp-title"><p>RESTAURANT TRADITIONNEL LE «T» SOMAIN</p></div>
-              <div className="exp-name"><p>Stage serveuse</p></div>
-              <div className="exp-description"><p>Serveuse</p></div>
+              <div className="exp-title">
+                <p>RESTAURANT TRADITIONNEL LE «T» SOMAIN</p>
+              </div>
+              <div className="exp-name">
+                <p>Stage serveuse</p>
+              </div>
+              <div className="exp-description">
+                <p>Serveuse</p>
+              </div>
             </div>
           </div>
           <div className="exp-main">
             <div className="exp-date">
-            {" "}
+              {" "}
               <p>
                 01/2012 <br /> 10/2011{" "}
               </p>
             </div>
             <div className="exp-content">
-              <div className="exp-title"><p>MAIRIE DE HORNAING</p></div>
-              <div className="exp-name"><p>ORNAING
-Stage accueil</p></div>
-              <div className="exp-description"><p>Accueil</p></div>
+              <div className="exp-title">
+                <p>MAIRIE DE HORNAING</p>
+              </div>
+              <div className="exp-name">
+                <p>ORNAING Stage accueil</p>
+              </div>
+              <div className="exp-description">
+                <p>Accueil</p>
+              </div>
             </div>
           </div>
           <div className="exp-main">
             <div className="exp-date">
-            {" "}
+              {" "}
               <p>
                 09/2011 <br /> 07/2011{" "}
               </p>
             </div>
             <div className="exp-content">
-              <div className="exp-title"><p>MAIRIE DE FENAIN</p></div>
-              <div className="exp-name"><p>Stage accueil</p></div>
-              <div className="exp-description"><p>Acueil</p></div>
+              <div className="exp-title">
+                <p>MAIRIE DE FENAIN</p>
+              </div>
+              <div className="exp-name">
+                <p>Stage accueil</p>
+              </div>
+              <div className="exp-description">
+                <p>Acueil</p>
+              </div>
             </div>
           </div>
           <div className="exp-main">
             <div className="exp-date">
-            {" "}
+              {" "}
               <p>
                 06/2011 <br /> 05/2011{" "}
               </p>
             </div>
             <div className="exp-content">
-              <div className="exp-title"><p>COLLEGE VICTOR HUGO SOMAIN</p></div>
-              <div className="exp-name"><p>Stage secrétariat</p></div>
-              <div className="exp-description"><p>Secrétariat</p></div>
+              <div className="exp-title">
+                <p>COLLEGE VICTOR HUGO SOMAIN</p>
+              </div>
+              <div className="exp-name">
+                <p>Stage secrétariat</p>
+              </div>
+              <div className="exp-description">
+                <p>Secrétariat</p>
+              </div>
             </div>
           </div>
           <div className="exp-main">
             <div className="exp-date">
-            {" "}
+              {" "}
               <p>
                 03/2011 <br /> 02/2011{" "}
               </p>
             </div>
             <div className="exp-content">
-              <div className="exp-title"><p>MAIRIE DE SOMAIN</p></div>
-              <div className="exp-name"><p>Stage secrétariat</p></div>
-              <div className="exp-description"><p>Secrétariat</p></div>
+              <div className="exp-title">
+                <p>MAIRIE DE SOMAIN</p>
+              </div>
+              <div className="exp-name">
+                <p>Stage secrétariat</p>
+              </div>
+              <div className="exp-description">
+                <p>Secrétariat</p>
+              </div>
             </div>
           </div>
         </div>
