319d6839eb1e97cadd2d9fbaec7a350105e916d1
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-10-09 12:20:01 +0200

    :FEAT: => Mise en place partie exp

--- src/pages/Exp.tsx
+++ src/pages/Exp.tsx
@@ -16,6 +16,95 @@ const Exp = () => {
       <Navbar />
       <div className="exp-txt">
         <h2> EXPERIENCE PROFESSIONNELLE</h2>
+        <div className="exp-container">
+          <div className="exp-main">
+            <div className="exp-date">
+              <p>
+                10/2022 <br /> 12/2018{" "}
+              </p>
+            </div>
+            <div className="exp-content">
+              <div className="exp-title">
+                <p>AMAZON LIL8</p>
+              </div>
+              <div className="exp-name">
+                <p>Agent de tri</p>
+              </div>
+              <div className="exp-description">
+                <p>
+                  Transport - Logistique - CACES 1A <br /> Instructeur,
+                  formation aux postes des intérimaires. - SST
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date">              <p>
+                10/2018 <br /> 12/2014{" "}
+              </p></div>
+            <div className="exp-content">
+              <div className="exp-title"><p>L’AUBERGE DU CHEVAL BLANC Restaurant 2** / Hôtel 3***</p></div>
+              <div className="exp-name"><p>Employée polyvalente</p></div>
+              <div className="exp-description"><p>Commis de salle / Plongeuse <br /> Femme de chambre</p></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+          <div className="exp-main">
+            <div className="exp-date"></div>
+            <div className="exp-content">
+              <div className="exp-title"></div>
+              <div className="exp-name"></div>
+              <div className="exp-description"></div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="second">
         <Footer />
